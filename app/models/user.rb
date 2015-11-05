d0222e7eadfe3ab2524fff2a8f43a201a7a8438b
ianwessen <ianwessen@gmail.com>
2015-11-04 18:31:30 -0800

    User model added; names, email, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 # User class
 class User < ActiveRecord::Base
-  validates :first_name, presence: true,
-                         length: { in: 2..32 }
 
-  validates :last_name, presence: true,
-                        length: { in: 2..32 }
+  before_save { self.email = email.downcase }
+
+  has_secure_password
 
   validates :email, presence: true,
                     length: { maximum: 64 },
                     uniqueness: true
+
+  validates :password, presence: true,
+                       length: { minimum: 6 }
+
+  validates :last_name, presence: true,
+                        length: { in: 2..32 }
+
+  validates :first_name, presence: true,
+                         length: { in: 2..32 }
+
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,9 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(
       first_name: 'Example',
       last_name: 'User',
-      email: 'user@example.com'
+      email: 'user@example.com',
+      password: 'foobar',
+      password_confirmation: 'foobar'
     )
   end
 
@@ -45,5 +47,16 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  ##### Password
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
