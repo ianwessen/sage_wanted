db17cb33032f01871989a65ff647cd76e55a992a
ianwessen <ianwessen@gmail.com>
2015-11-15 17:46:59 -0800

    destructive removal of SignUps

--- app/models/sign_up.rb
+++
@@ -1,9 +0,0 @@
-class SignUp < ActiveRecord::Base
-
-  VALID_EMAIL_REGEX = /.+@.+\..+/i
-
-  validates_presence_of :email
-  validates_uniqueness_of :email, :message => "is already registered"
-  validates_format_of :email, :with => VALID_EMAIL_REGEX, :message => "format is invalid"
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20151113202342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "sign_ups", force: :cascade do |t|
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
